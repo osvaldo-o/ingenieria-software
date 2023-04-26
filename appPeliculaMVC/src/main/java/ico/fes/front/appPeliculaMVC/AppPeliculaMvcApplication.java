17a9ab27dcfbb3668b4266a1157d39d9daf473ed
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-04-25 23:37:24 -0600

    init mvc

---
+++ appPeliculaMVC/src/main/java/ico/fes/front/appPeliculaMVC/AppPeliculaMvcApplication.java
@@ -0,0 +1,13 @@
+package ico.fes.front.appPeliculaMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPeliculaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPeliculaMvcApplication.class, args);
+	}
+
+}
---
+++ appPeliculaMVC/src/test/java/ico/fes/front/appPeliculaMVC/AppPeliculaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ico.fes.front.appPeliculaMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPeliculaMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
